eb2cfb461e64e84d5c9fbf5fd69dc9cfdf64230d
jaravan <aravanis.yannis@gmail.com>
2023-03-27 00:58:02 +0300

    wallet listener

--- src/Minter.js
+++ src/Minter.js
@@ -13,10 +13,13 @@ const Minter = (props) => {
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
  
+  // Wallet functionality =======>
   useEffect(async () => { //TODO: implement
     const {address, status} = await getCurrentWalletConnected();
     setWallet(address)
     setStatus(status); 
+
+    addWalletListener(); 
   }, []);
 
   const connectWalletPressed = async () => { //TODO: implement
@@ -24,6 +27,32 @@ const Minter = (props) => {
     setStatus(walletResponse.status);
     setWallet(walletResponse.address);
   };
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+  // Wallet functionality <=======
 
   const onMintPressed = async () => { //TODO: implement
     
